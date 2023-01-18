4836330a33d789bd73f5d1289c53c7344528d964
Patrick Mazara <patrickmazara@hotmail.com>
2023-01-18 18:03:24 -0500

    edit songSchema

--- models/song.js
+++ models/song.js
@@ -34,7 +34,7 @@ const songSchema = new Schema({
   genre: {
     type: String,
     required: true,
-    enum: ['hip-hop-rap', 'r-and-b', 'rock', 'pop', 'country', 'jazz', 'heavy Metal', 'classical', 'indie', 'reggaeton', 'electronic', 'latin']
+    enum: ['Hip-Hop', 'Rhythm-and-Blues', 'Rock', 'Pop', 'Country', 'Jazz', 'Heavy Metal', 'Classical', 'Indie', 'Reggaeton', 'Electronic', 'Latin']
   },
   explicitRating: {
     type: Boolean, 
